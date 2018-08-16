05f7d8c6c769f3ddda5f0fc0d355f7ef2338cfb7
Timer91 <marais.jeremie@gmail.com>
2018-08-16 02:18:08 +0200

    [HTML/JS] Read file dropped, display name and duration in min:sec
    - check the dropped file extension: ".gs"
    - read the file
    - display its name and duration replacing actual span text

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,38 @@
+function beatToTime( b, bpm ) {
+	let s = b / bpm * 60;
+
+	return ( s - ( s %= 60 ) ) / 60 + ( 9 < s ? ':' : ':0' ) + ~~s;
+}
+
+function displayFile( e ) {
+	const fileContent = JSON.parse( e.target.result ),
+		elTxt = document.querySelector( "span" );
+
+	elTxt.innerHTML = `<b>${fileContent.name}</b> - 
+		<b>${beatToTime(fileContent.duration, fileContent.bpm)}</b>`;
+}
+
+function getFile( blob ) {
+	const f = new FileReader();
+
+	f.onload = displayFile.bind();
+	f.readAsText( blob );
+}
+
 function dropHandler( e ) {
-	const files = e.dataTransfer.items || e.dataTransfer.files;
+	const files = e.dataTransfer.items || e.dataTransfer.files,
+		file = files[ 0 ];
 
 	e.preventDefault();
+	if ( file.kind === "file" ) {
+		const blob = file.getAsFile();
 
-	for ( let i = 0; i < files.length; i++ ) {
-		if ( files[ i ].kind === "file" ) {
-			const file = files[ i ].getAsFile();
-
-			console.log( ` .. file[${i}].name = ${file.name}` );
+		if ( blob.name.substr( -3 ) === ".gs" ) {
+			getFile( blob );
+		} else {
+			this.classList.remove( "dragover" );
 		}
 	}
-
-	this.classList.remove( "dragover" );
 
 	return false;
 }
