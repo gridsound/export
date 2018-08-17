e8a320650e28fdb502d1d13ddbd17d02e11a46b8
Timer91 <marais.jeremie@gmail.com>
2018-08-17 04:18:10 +0200

    index.js: instanciate just DragAndDrop

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,2 @@
-const dad = new DragAndDrop( document.getElementById( "dropbox" ) ),
-	render = new Render(),
-	gswasynth = new gswaSynth(),
-	gswasched = new gswaScheduler();
+const elDropBox = document.querySelector( "#dropbox" ),
+	dragAndDrop = new DragAndDrop( elDropBox );
